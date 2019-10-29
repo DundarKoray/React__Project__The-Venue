9279beded7bf7b6608b0fade360382e41f0ec3f9
Koray Dundar <koraydundar_40@hotmail.com>
2019-10-29 12:54:28 +0200

    completed venue info section

--- src/components/venueInfo/index.js
+++ src/components/venueInfo/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Zoom from 'react-reveal/Zoom'
+
 import icon_calendar from '../../resources/images/icons/calendar.png'
 import icon_location from '../../resources/images/icons/location.png'
 
@@ -7,23 +9,44 @@ const VenueInfo = () => {
         <div className="bck_black">
             <div className="center_wrapper">
                 <div className="vn_wrapper">
-                    <div className="vn_item">
-                        <div className="vn_outer">
-                            <div className="vn_inner">
-                                <div className="vn_icon_square bck_red"></div>
-                                <div
-                                    className="vn_icon"
-                                    style={{ background: `url(${icon_calendar})` }}
-                                ></div>
-                                <div className="vn_title">
-                                    Event Date & Time
+                    <Zoom duration={500}>
+                        <div className="vn_item">
+                            <div className="vn_outer">
+                                <div className="vn_inner">
+                                    <div className="vn_icon_square bck_red"></div>
+                                    <div
+                                        className="vn_icon"
+                                        style={{ background: `url(${icon_calendar})` }}
+                                    ></div>
+                                    <div className="vn_title">
+                                        Event Date & Time
                                 </div>
-                                <div className="vn_desc">
-                                    31 December 2019 @10.00 pm
+                                    <div className="vn_desc">
+                                        31 December 2019 @10.00 pm
+                                </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </Zoom>
+                    <Zoom delay={500} duration={500}>
+                        <div className="vn_item">
+                            <div className="vn_outer">
+                                <div className="vn_inner">
+                                    <div className="vn_icon_square bck_red"></div>
+                                    <div
+                                        className="vn_icon"
+                                        style={{ background: `url(${icon_location})` }}
+                                    ></div>
+                                    <div className="vn_title">
+                                        Event Location
+                                </div>
+                                    <div className="vn_desc">
+                                        Helsinki Olympic Stadium
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Zoom>
                 </div>
             </div>
         </div>
