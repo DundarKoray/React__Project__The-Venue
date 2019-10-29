34c530ffbd76b0b38bead2b46f6dd27a575be4d9
Koray Dundar <koraydundar_40@hotmail.com>
2019-10-29 13:43:10 +0200

    completed highlight section, used react reveal

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
 
 class Discount extends Component {
+
+    state = {
+        discountStart: 0,
+        discountEnd: 30
+    }
+
+    porcentage = () => {
+        if (this.state.discountStart < this.state.discountEnd) {
+            this.setState({
+                discountStart: this.state.discountStart + 1
+
+            })
+        }
+    }
+
+    componentDidUpdate() {
+        setTimeout(() => {
+            this.porcentage()
+        }, 30)
+    }
+
     render() {
         return (
             <div className="center_wrapper">
                 <div className="discount_wrapper">
-                    <div className="discount_porcentage">
-                        <span>23%</span>
-                        <span>OFF</span>
-                    </div>
-                    <div className="discount_description">
-                        <h3>Purchase tickets before 20th NOVEMBER</h3>
-                        <p></p>
-                    </div>
+                    <Fade onReveal={() => this.porcentage()}>
+                        <div className="discount_porcentage">
+                            <span>{this.state.discountStart}%</span>
+                            <span>OFF</span>
+                        </div>
+                    </Fade>
+                    <Slide right>
+                        <div className="discount_description">
+                            <h3>Purchase tickets before 20th NOVEMBER</h3>
+                            <p></p>
+                            <div>button</div>
+                        </div>
+                    </Slide>
                 </div>
             </div>
         );
