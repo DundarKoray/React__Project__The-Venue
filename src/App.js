6f78e9a8ef01f6440ae89b68287353e2c9c3b5e9
Koray Dundar <koraydundar_40@hotmail.com>
2019-11-05 12:22:29 +0200

    completed footer and location section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Featured from './components/featured'
 import VenueInfo from './components/venueInfo'
 import Highlights from './components/highlights'
 import Pricing from './components/pricing'
+import Location from './components/location'
+import Footer from './components/header_footer/Footer'
 
 import './resources/styles.css';
 
@@ -18,6 +20,8 @@ class App extends Component {
         <VenueInfo />
         <Highlights />
         <Pricing />
+        <Location />
+        <Footer />
       </div>
     );
   }
