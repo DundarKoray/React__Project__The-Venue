6966898f733a1837e960e6002e12f9160bcf5fe7
Koray Dundar <koraydundar_40@hotmail.com>
2020-03-27 12:14:03 +0200

    changed deadline time for the countdown

--- src/components/featured/CountDown.js
+++ src/components/featured/CountDown.js
@@ -4,7 +4,7 @@ import Slide from 'react-reveal/Slide';
 class CountDown extends Component {
 
     state = {
-        deadline: 'Dec, 31, 2019',
+        deadline: 'Dec, 31, 2020',
         days: 0,
         hours: 0,
         minutes: 0,
